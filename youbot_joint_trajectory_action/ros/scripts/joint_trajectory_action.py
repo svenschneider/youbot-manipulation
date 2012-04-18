5bf8e69d68c501e705f18cd13961ee01391c2a36
Sven Schneider <sschne2s@smail.inf.h-brs.de>
2012-04-19 00:04:23 +0200

    joint trajectory action node now handles trajectories with more than one point

--- youbot_joint_trajectory_action/ros/scripts/joint_trajectory_action.py
+++ youbot_joint_trajectory_action/ros/scripts/joint_trajectory_action.py
@@ -2,7 +2,6 @@
 import roslib; roslib.load_manifest('youbot_joint_trajectory_action')
 
 import rospy
-import threading
 import sensor_msgs.msg
 import actionlib
 import brics_actuator.msg
@@ -12,7 +11,6 @@ import control_msgs.msg
 class JointTrajectoryAction:
 	
 	def __init__(self):
-		self.lock = threading.Lock()
 		self.received_state = False
 		
 		if (not rospy.has_param("joint_trajectory_action/unit")):
@@ -31,50 +29,53 @@ class JointTrajectoryAction:
 		rospy.loginfo("Unit: %s", self.unit)
 		
 		# subscriptions
-		rospy.Subscriber('joint_states', sensor_msgs.msg.JointState, self.joint_states_callback)
+		rospy.Subscriber("joint_states", sensor_msgs.msg.JointState, self.joint_states_callback)
 		
-		self.pub = rospy.Publisher('position_command', brics_actuator.msg.JointPositions)
+		self.pub = rospy.Publisher("position_command", brics_actuator.msg.JointPositions)
 		
 		self.action = actionlib.SimpleActionServer("joint_trajectory_action", control_msgs.msg.FollowJointTrajectoryAction, execute_cb = self.execute_cb)
 	
 	
 	def joint_states_callback(self, msg):
-		self.lock.acquire()
 		for k in range(len(self.joint_names)):
 			for i in range(len(msg.name)):
 				if (msg.name[i] == self.joint_names[k]):
 					self.configuration[k] = msg.position[i]
 		self.received_state = True
-		self.lock.release()
 
 	
 	def execute_cb(self, goal):
-		joint_positions = brics_actuator.msg.JointPositions()
-		conf = goal.trajectory.points[0].positions
-		
-		# transform from ROS to BRICS message
-		for i in range(len(self.joint_names)):
-			joint_value = brics_actuator.msg.JointValue()
-			joint_value.joint_uri = self.joint_names[i]
-			joint_value.value = conf[i]
-			joint_value.unit = self.unit
-			joint_positions.positions.append(joint_value)
-		self.pub.publish(joint_positions)
-		
-		# wait to reach the goal position
 		is_timed_out = False
 		start = rospy.Time.now()
 		duration = rospy.Duration(5.0)
-		while True:
-			if (self.is_goal_reached(conf, self.configuration)):
-				break
-			if (rospy.Time.now() - start > duration):
-				is_timed_out = True
-				break
 		
+		for i in range(len(goal.trajectory.points)):
+			joint_positions = brics_actuator.msg.JointPositions()
+			conf = goal.trajectory.points[i].positions
+			
+			# transform from ROS to BRICS message
+			for i in range(len(self.joint_names)):
+				joint_value = brics_actuator.msg.JointValue()
+				joint_value.joint_uri = self.joint_names[i]
+				joint_value.value = conf[i]
+				joint_value.unit = self.unit
+				joint_positions.positions.append(joint_value)
+			self.pub.publish(joint_positions)
+			
+			# wait to reach the goal position
+			while (not rospy.is_shutdown()):
+				if (self.is_goal_reached(conf, self.configuration)):
+					break
+				if (rospy.Time.now() - start > duration):
+					is_timed_out = True
+					break
+			
+			if (is_timed_out):
+				break
+			
 		result = control_msgs.msg.FollowJointTrajectoryResult()
 		if (is_timed_out):
-			result.error_code = control_msgs.msg.FollowJointTrajectoryResult.INVALID_GOAL
+			result.error_code = control_msgs.msg.FollowJointTrajectoryResult.PATH_TOLERANCE_VIOLATED
 			self.action.set_aborted(result)
 		else:
 			result.error_code = control_msgs.msg.FollowJointTrajectoryResult.SUCCESSFUL
@@ -89,7 +90,7 @@ class JointTrajectoryAction:
 
 
 if __name__ == "__main__":
-	rospy.init_node('joint_trajectory_action')
+	rospy.init_node("joint_trajectory_action")
 	rospy.sleep(0.5)
 	
 	action = JointTrajectoryAction()
