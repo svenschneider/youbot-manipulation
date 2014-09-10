ffd3aefd75feae0772f25141094b85a932a317d1
Sven Schneider <sven.schneider@h-brs.de>
2014-09-10 22:26:16 +0000

    Adapt to changed logging interface in ROS Indigo.

--- youbot_arm_kinematics_moveit/ros/src/kinematics_logger.cpp
+++ youbot_arm_kinematics_moveit/ros/src/kinematics_logger.cpp
@@ -13,8 +13,8 @@ void KinematicsLogger::write(const std::string &msg, const char *file, int line)
     ROSCONSOLE_DEFINE_LOCATION(true, ros::console::levels::Debug,
             ROSCONSOLE_ROOT_LOGGER_NAME ".youbot_arm_kinematics");
 
-    if (enabled) {
-        ros::console::print(0, loc.logger_, ros::console::levels::Debug,
-                file, line, "", "%s", msg.c_str());
+    if (__rosconsole_define_location__loc.logger_enabled_) {
+        ros::console::print(0, __rosconsole_define_location__loc.logger_,
+                ros::console::levels::Debug, file, line, "", "%s", msg.c_str());
     }
 }
